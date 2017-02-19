059fb935491a30c0fa25e7dbb00aff75616a4552
Arflog <ksenia43rus@bk.ru>
2017-02-19 16:38:25 +0300

    project

---
+++ js/script.js
@@ -0,0 +1,11 @@
+var btn = document.querySelector(".btn-search");
+      var form = document.querySelector(".search-form");
+      btn.addEventListener("click", function(event) {
+        event.preventDefault();
+        if(form.classList.contains("form-open")){
+          form.classList.remove("form-open");
+        }
+        else {
+          form.classList.add("form-open");
+        }
+      });
