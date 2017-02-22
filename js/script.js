bbc344b4f6c84e79154c9ff457846cdd9bd07d49
Arflog <ksenia43rus@bk.ru>
2017-02-22 12:54:12 +0300

    int

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 var btn = document.querySelector(".link-search");
-      var form = document.querySelector(".search-form");
-      btn.addEventListener("click", function(event) {
-        event.preventDefault();
-        if(form.classList.contains("form-open")){
-          form.classList.remove("form-open");
-        }
-        else {
-          form.classList.add("form-open");
-        }
-      });
+var form = document.querySelector(".search-form");
+  btn.addEventListener("click", function(event) {
+  event.preventDefault();
+  if(form.classList.contains("form-open")){
+    form.classList.remove("form-open");
+    }
+    else {
+      form.classList.add("form-open");
+      }
+  });
