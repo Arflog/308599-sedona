35e162fd5fec8f2b69610fec079789264d177574
Arflog <ksenia43rus@bk.ru>
2017-02-22 16:15:40 +0300

    sedona project

--- js/script.js
+++ js/script.js
@@ -1,11 +1,10 @@
 var btn = document.querySelector(".link-search");
 var form = document.querySelector(".search-form");
-  btn.addEventListener("click", function(event) {
+btn.addEventListener("click", function (event) {
   event.preventDefault();
-  if(form.classList.contains("form-open")){
+  if (form.classList.contains("form-open")) {
     form.classList.remove("form-open");
-    }
-    else {
-      form.classList.add("form-open");
-      }
-  });
+  } else {
+    form.classList.add("form-open");
+  }
+});
